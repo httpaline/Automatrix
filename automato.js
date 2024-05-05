5c8085ac4c7eedda6a242acba5017e51fc053c31
Aline <91509515+httpaline@users.noreply.github.com>
2024-05-05 13:38:53 -0300

    Add files via upload

---
+++ automato.js
@@ -0,0 +1,8 @@
+class Automato {
+    constructor(estados, transicoes, estadoInicial, estadosAceitos ){
+        this.estados=estados;
+        this.transicoes=transicoes;
+        this.estadoInicial=estadoInicial;
+        this.estadosAceitos=estadosAceitos;
+    }
+}
